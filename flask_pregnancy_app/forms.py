942898a16864e70ba6ff651014349522a79c80a3
aniakr <anna.krach@gmail.com>
2019-07-30 22:37:38 +0200

    pregnancy app - additional calculations within the app

--- flask_pregnancy_app/forms.py
+++ flask_pregnancy_app/forms.py
@@ -1,7 +1,16 @@
 from flask_wtf import Form
 from wtforms.fields.html5 import DateField
-from wtforms import SubmitField
+from wtforms import SubmitField, IntegerField,validators
+from wtforms.validators import DataRequired
 
 class LmpInputForm(Form):
-    lmp=DateField('Date of the beginning of your last period', format='%Y-%m-%d')
-    calculations = SubmitField("Check the dates")
+    lmp=DateField('Date of the beginning of your last period', format='%Y-%m-%d',validators=[DataRequired()])
+    calculations = SubmitField("Check your current week and edd")
+
+class GivenWeekCalculation(Form):
+    required_week=IntegerField('Week that interests you',[validators.number_range(min=1,max=40)])
+    calculations = SubmitField("When that week will come")
+
+class GivenDateCalculation(Form):
+    required_date=DateField('Enter the date you would like to check', format='%Y-%m-%d')
+    calculations = SubmitField("Which week will be at that date")
--- flask_pregnancy_app/pregnancy_application.py
+++ flask_pregnancy_app/pregnancy_application.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request,url_for, flash
 from Pregnancy_calendar.calculator.pregnancy_calendar import PregnancyCalendar
-from Pregnancy_calendar.flask_pregnancy_app.forms import LmpInputForm
+from Pregnancy_calendar.flask_pregnancy_app.forms import LmpInputForm, GivenWeekCalculation, GivenDateCalculation
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='Secret'
@@ -28,12 +28,23 @@ def about():
 @app.route("/calendar",methods=['GET','POST'])
 def calendar():
     form=LmpInputForm()
+    form2=GivenWeekCalculation()
+    form3=GivenDateCalculation()
     if request.method=='POST':
         if form.validate_on_submit():
-            my_pregnancy=PregnancyCalendar(form.lmp.data.strftime('%m/%d/%y'))
+            my_pregnancy = PregnancyCalendar(form.lmp.data.strftime('%m/%d/%y'))
             flash(f'You are in {my_pregnancy.current_week()[0]} week and {my_pregnancy.current_week()[1]} day')
             flash(f'Your edd is {my_pregnancy.edd_calculator()}')
-    return render_template("calendar.html", title='Calendar',form=form)
+        if form2.validate_on_submit():
+            my_pregnancy = PregnancyCalendar(form.lmp.data.strftime('%m/%d/%y'))
+            flash(f'Week {form2.required_week.data} will start on '
+                  f'{my_pregnancy.given_week_calculator(form2.required_week.data)}')
+        if form3.validate_on_submit():
+            my_pregnancy = PregnancyCalendar(form.lmp.data.strftime('%m/%d/%y'))
+            flash(f'On {form3.required_date.data} you will be in '
+                f'{my_pregnancy.week_at_given_date(form3.required_date.data.strftime("%m/%d/%y"))[0]} week and '
+                f'{my_pregnancy.week_at_given_date(form3.required_date.data.strftime("%m/%d/%y"))[1]} day')
+    return render_template("calendar.html", title='Calendar',form=form,form2=form2,form3=form3)
 
 if __name__=="__main__":
     app.run(debug=True)
